ba1658bf8a471f56353a08c78934cd218dcf2e04
Emmanuel Guido <eguido@csumb.edu>
2018-02-01 01:28:06 +0000

    Added Files

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset= "utf-8" />
+        <title>Emmanuel Guido: Personal Website</title>
+        <link href= "css/stylez.css" rel= "stylesheet" type= "text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header>
+            <h1>Emmanuel Guido</h1>
+        </header>   
+        
+        <nav>
+            <hr width= "50%"/>   
+            <a href= "index.php" id= "current">Home</a>
+            <a href= "about.html">About</a>
+            <a href= "contact.html">Contact</a>
+        </nav>
+        
+        <br /> <br />
+        
+        <main>
+            <figure id= "me">
+                <img src= "img/profilePictureCropped.png" alt= "Picture of Emmanuel Guido" />
+            </figure>
+            
+            <div id="welcomeText">
+                Hello!<br />
+                <p>Thank You for visiting my website!</p>
+                <P>I am a Software Engineering student at CSUMB.</p>
+                <p>Feel free to contact me!</p>
+                
+                <br /> <br />
+                <div id= "quote">
+                <em>
+                    "We’d like to be painters, we’d like to be poets.<br /> 
+                    We’d like to be writers, but as everybody knows<br />
+                    —we can’t earn any money that way. What do you want to do?<br /> 
+                    When we finally got down to something which the individual says he really wants to do,<br /> 
+                    I will say to him you do that—and uhm—forget the money.<br />
+                    If you say that getting the money is the most important thing,<br /> 
+                    you will spend your life completely wasting your time..."
+                </em>
+                <br />
+                -Alan Watts
+                </div>
+            </div>
+        </main>
+        
+         <!--This is the footer -->
+         <!--The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST336-Internet Programing. 2018&copy; Guido<br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous.<br />
+            It is used for academic purposes only.
+            <figure id="otter">
+                <img src= "img/csumbLogo120px.png" alt= "CSUMB Logo"/>
+            </figure>
+        </footer>
+        
+        
+        
+    </body>
+
+</html>
